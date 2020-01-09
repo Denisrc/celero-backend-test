8eb0fad17c16406d8388619ec6831fd9630ebbd8
João Denis Rodrigues Cabral <joao.denisr@gmail.com>
2020-01-09 15:19:58 -0300

    created model, serializers and views for Sports

--- celero/olympics/models.py
+++ celero/olympics/models.py
@@ -1,1 +1,4 @@
 from django.db import models
+
+class Sport(models.Model):
+    name = models.CharField(blank=False, max_length=100, null=False, unique=True)
--- celero/olympics/views.py
+++ celero/olympics/views.py
@@ -1,1 +1,10 @@
 from django.shortcuts import render
+from olympics.models import Sport
+from olympics.serializers import SportSerializer
+from rest_framework.parsers import JSONParser
+from rest_framework.views import APIView
+from rest_framework import viewsets
+
+class SportViewset(viewsets.ModelViewSet):
+    queryset = Sport.objects.all()
+    serializer_class = SportSerializer
